17987e4994f7ce1f4809de5e182a7d551e2aeed6
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-08-02 13:01:23 -0600

    jdbc mysql connection

--- web-service/src/main/java/com/victortello/ws/webservice/UserServiceImp.java
+++ web-service/src/main/java/com/victortello/ws/webservice/UserServiceImp.java
@@ -1,14 +1,27 @@
 package com.victortello.ws.webservice;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImp implements UserService{
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDto createUser(UserDto user) {
-        
-        return null;
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId("testUserId");
+        UserEntity storeUserDetails = userRepository.save(userEntity);
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(storeUserDetails, returnValue);
+
+        return returnValue;
     }
     
 }
