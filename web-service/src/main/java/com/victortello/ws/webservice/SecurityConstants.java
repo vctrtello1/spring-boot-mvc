c28e20411ce5bdde17d7fd51e13de186d10c99b9
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-08-05 09:18:41 -0600

    fix token prefix name

--- web-service/src/main/java/com/victortello/ws/webservice/SecurityConstants.java
+++ web-service/src/main/java/com/victortello/ws/webservice/SecurityConstants.java
@@ -3,7 +3,7 @@ package com.victortello.ws.webservice;
 public class SecurityConstants {
 
     public static final long EXPIRATION_TIME = 3600000; // 1 hour
-    public static final String TOKEN_PREFIX = " Barear ";
+    public static final String TOKEN_PREFIX = "Barear ";
     public static final String SIGN_UP_URL = "/users";
     public static final String TOKEN_SECRET = "adp3jdu83u7sdh";
     public static final String HEADER_STRING = "authorization";
