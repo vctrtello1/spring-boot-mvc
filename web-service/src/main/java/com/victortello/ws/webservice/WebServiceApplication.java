adaaa156e15fd6cde7d1c4e25b82c6a80b0c7a91
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-08-09 14:55:55 -0600

    generate war files

--- web-service/src/main/java/com/victortello/ws/webservice/WebServiceApplication.java
+++ web-service/src/main/java/com/victortello/ws/webservice/WebServiceApplication.java
@@ -2,11 +2,18 @@ package com.victortello.ws.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class WebServiceApplication {
+public class WebServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebServiceApplication.class, args);
