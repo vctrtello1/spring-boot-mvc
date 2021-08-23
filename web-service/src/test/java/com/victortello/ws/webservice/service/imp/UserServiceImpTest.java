a9257eeb564987f950c266e01e01270977a358b5
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-08-23 10:15:40 -0500

    remove verification service

---
+++ web-service/src/test/java/com/victortello/ws/webservice/service/imp/UserServiceImpTest.java
@@ -0,0 +1,27 @@
+package com.victortello.ws.webservice.service.imp;
+
+import com.victortello.ws.webservice.io.repository.UserRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+class UserServiceImpTest {
+
+	@Mock
+	UserRepository userRepository;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+
+	@Test
+	void testGetUser() {
+		fail("Not yet implemented");
+
+	}
+
+}
