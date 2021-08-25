c5351e0b6b395c9c8c59b8cf072ec2d70a094f9c
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-08-25 11:49:15 -0500

    test jwt approved

--- web-service/src/test/java/com/victortello/ws/shared/UtilsTest.java
+++ web-service/src/test/java/com/victortello/ws/shared/UtilsTest.java
@@ -3,7 +3,6 @@ package com.victortello.ws.shared;
 import com.victortello.ws.webservice.shared.Utils;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +11,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.junit.jupiter.api.Assertions.assertFalse;
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(classes= com.victortello.ws.webservice.shared.Utils.class)
 class UtilsTest {
 
 	@Autowired
@@ -37,9 +36,22 @@ class UtilsTest {
 	}
 
 	@Test
-	@Disabled
 	void testHasTokenExpired() {
 
+		String expiredToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ2Y3RydGVsbG9AZ21haWwuY29tIiwiZXhwIjoxNjI5OTA0NDcyfQ.mydOmh0avgznNYVZRmfxzkh7Yob-3eEQFPHvDtmCXREYV1LnMhM8aZqQ7lftSXEFSmOiPxe_04jkaZyVNmvAbQ";
+		boolean hasTokenExpired = Utils.hasTokenExpired(expiredToken);
+
+		assertTrue(hasTokenExpired);
+
+	}
+
+	@Test
+	final void testHasTokenNotExpired() {
+		String token = utils.generateEmailVerificationToken("4yr65hhyid84");
+		assertNotNull(token);
+
+		boolean hasTokenExpired = Utils.hasTokenExpired(token);
+		assertFalse(hasTokenExpired);
 	}
 
 }
